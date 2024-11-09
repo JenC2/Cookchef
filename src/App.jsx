c2ceaa998638e8f9733c7ad1ef61ffb8586a7dd5
jennychen <jenny.chenchou@gmail.com>
2024-11-09 22:56:32 +0100

    set up header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import styles from "./App.module.scss";
 
 function App() {
   return (
-      <div>
-        <h1>Cookchef</h1>
-      </div>
-  )
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      {/* <Content /> */}
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
