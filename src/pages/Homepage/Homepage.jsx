b7568b4f4d276eff3fd4b1e59d24ac54f1bf412a
jennychen <jenny.chenchou@gmail.com>
2024-12-07 22:04:25 +0100

    add function to fetch recipes

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,36 @@
 import Loading from "../../components/Loading/Loading";
+import { ApiContext } from "../../context/ApiContext";
 import styles from "./Homepage.module.scss";
 import Recipe from "./components/Recipe/Recipe";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 function Homepage() {
   const [recipes, setRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [filter, setFilter] = useState("");
+  const BASE_URL_API = useContext(ApiContext);
+
+  useEffect(() => {
+    let cancel = false;
+    async function fetchRecipes() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(BASE_URL_API);
+        if (response.ok && !cancel) {
+          const recipes = await response.json();
+          setRecipes(Array.isArray(recipes) ? recipes : [recipes]);
+        }
+      } catch (e) {
+        console.log("Error", e);
+      } finally {
+        if (!cancel) {
+          setIsLoading(false);
+        }
+      }
+    }
+    fetchRecipes();
+    return () => (cancel = true);
+  }, []);
 
   function handleInput(e) {
     const filter = e.target.value;
